d5642f71298e153aadf13619fff54c0849fd2f86
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-22 17:56:21 +0530

    map filster use

--- expense-tracker-react/src/App.jsx
+++ expense-tracker-react/src/App.jsx
@@ -2,30 +2,49 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+
+  function addTransaction() {
+    console.log("Add Transaction");
+  }
 
   return (
     <>
       <h1>Expense Tracker</h1>
 
       <input
-  type="text"
-  placeholder="Description"
-  value={description}
-  onChange={(e) => setDescription(e.target.value)}
-/>
-      <br /><br />
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <br />
+      <br />
 
       <input
         type="number"
         placeholder="Amount"
-
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
       />
- <br /><br />
 
-      <button onClick={addTransaction}></button>
+      <br />
+      <br />
+
+      <button onClick={addTransaction}>
+        Add Transaction
+      </button>
+
+      <div>
+        {transactions.map((item) => (
+          <p key={item.id}>
+            {item.description} - ₹{item.amount}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
