3b6a444afd21eab89b20269e3ff9569c3286e674
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-22 15:11:04 +0530

    Add dynamic balance display

--- expense-tracker-react/src/App.jsx
+++ expense-tracker-react/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 
 function App() {
 
+  let balance = 1000;
 
   return (
     <>
       <h1 className="title">Expense Tracker</h1>
-      <h2>Balance : ₹0</h2>
+      <h2>Balance : ₹ {balance}</h2>
       <button>Add Transaction</button>
     </>
-)
+  )
 }
 
 export default App
