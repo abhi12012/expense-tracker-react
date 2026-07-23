c9339ea5f1917376179894d3917a0af244221d21
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-23 19:31:46 +0530

    Refactor components using props destructuring

--- expense-tracker-react/src/components/Dashboard.jsx
+++ expense-tracker-react/src/components/Dashboard.jsx
@@ -1,14 +1,13 @@
-function Dashboard(props) {
+function Dashboard({ name, balance, income, expense }) {
   return (
     <>
       <h2>Dashboard Component</h2>
 
-      <p>Hello {props.name}</p>
+      <p>Hello {name}</p>
 
-      
-      <h2>Balance: ₹{props.balance}</h2>
-<h2>Income: ₹{props.income}</h2>
-<h2>Expense: ₹{props.expense}</h2>
+<h2>Balance: ₹{balance}</h2>
+<h2>Income: ₹{income}</h2>
+<h2>Expense: ₹{expense}</h2>
     </>
   );
 }
