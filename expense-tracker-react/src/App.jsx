c7b6baab8212fca0b1f7abc366e05338f3196099
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-27 21:43:39 +0530

    Add delete confirmation alert

--- expense-tracker-react/src/App.jsx
+++ expense-tracker-react/src/App.jsx
@@ -114,10 +114,19 @@ function updateTransaction() {
 
 
   function deleteTransaction(id) {
-    setTransactions(
-      transactions.filter((item) => item.id !== id)
-    );
+
+  const confirmDelete = window.confirm(
+    "Are you sure you want to delete this transaction?"
+  );
+
+  if (!confirmDelete) {
+    return;
   }
+
+  setTransactions(
+    transactions.filter((item) => item.id !== id)
+  );
+}
 
 
 
