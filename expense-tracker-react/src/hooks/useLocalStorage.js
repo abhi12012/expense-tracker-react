e703fbe9c08d8100c0623617ae2e55df5303bf55
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-28 18:56:50 +0530

    Store transactions using localStorage hook

--- expense-tracker-react/src/hooks/useLocalStorage.js
+++ expense-tracker-react/src/hooks/useLocalStorage.js
@@ -2,6 +2,28 @@ import { useState, useEffect } from "react";
 
 function useLocalStorage() {
 
+  const [transactions, setTransactions] = useState(() => {
+    const savedTransactions = localStorage.getItem("transactions");
+
+    return savedTransactions
+      ? JSON.parse(savedTransactions)
+      : [];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem(
+      "transactions",
+      JSON.stringify(transactions)
+    );
+  }, [transactions]);
+
+
+  return {
+    transactions,
+    setTransactions
+  };
+
 }
 
 export default useLocalStorage;
