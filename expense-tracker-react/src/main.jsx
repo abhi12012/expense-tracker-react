cd505fa510193c2031db7cd893adc868a75e3f72
abhi12012 <agrawalabhishek211@gmail.com>
2026-07-29 18:16:00 +0530

    Add pages structure for routing

--- expense-tracker-react/src/main.jsx
+++ expense-tracker-react/src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import { TransactionProvider } from "./context/TransactionContext";
 
 import { StrictMode } from 'react'
@@ -7,8 +8,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <TransactionProvider>
-    <App />
-  </TransactionProvider>
+  <BrowserRouter>
+    <TransactionProvider>
+      <App />
+    </TransactionProvider>
+  </BrowserRouter>
 </StrictMode>
 )
