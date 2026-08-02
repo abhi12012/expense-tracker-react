27a9c88bf77b6d9dbe3b1608d455e592916f59da
abhi12012 <agrawalabhishek211@gmail.com>
2026-08-02 11:44:10 +0530

    made decrement too browser seen

--- expense-tracker-react/src/components/Counter.jsx
+++ expense-tracker-react/src/components/Counter.jsx
@@ -2,11 +2,21 @@ import { useReducer } from "react";
 
 const initialState = 0;
 
+
+
+
 function reducer(state, action) {
 
   if (action === "increment") {
     return state + 1;
   }
+
+  if (action === "decrement") {
+  return state - 1;
+}
+
+return state;
+
 
 }
 
@@ -30,6 +40,17 @@ function Counter() {
 >
   +
 </button>
+
+
+<br />
+<br />
+
+<button
+  onClick={() => dispatch("decrement")}
+>
+  -
+</button>
+
   </>
 );
 
