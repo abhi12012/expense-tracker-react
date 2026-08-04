95d79dc1240304c94d3fef27dbf754af73dfb898
abhi12012 <agrawalabhishek211@gmail.com>
2026-08-04 13:19:17 +0530

    something more added

--- expense-tracker-react/src/components/DataManagement.jsx
+++ expense-tracker-react/src/components/DataManagement.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
+
+
 function DataManagement() {
+
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+     const handleFileRead = () => {
+
+  const reader = new FileReader();
+
+};
 
   return (
     <div className="data-management">
 
       <h2>Data Management</h2>
 
-      
-
-      <input
-  type="file"
-  accept=".csv"
-/>
-
 
 
       <input
   type="file"
   accept=".csv"
+  onChange={(e) => setSelectedFile(e.target.files[0])}
 />
+
+<p>
+  {selectedFile ? selectedFile.name : "No file selected"}
+</p>
+
+
+
+
+<button>
+        Import CSV
+      </button>
+
 
 
       <button>
